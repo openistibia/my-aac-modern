edfc8eb11261b3c392da15c43bacbe8176f44d8b
underewarrr <underewar@gmail.com>
2022-05-04 15:37:45 -0300

    Dev: Navbar, News, Players Highscore implemented

--- system/cache/twig/6b/6b0a1266876d8b714152b8c0c26d6497a287c66512861e21b4e4e437af04c117.php
+++ system/cache/twig/6b/6b0a1266876d8b714152b8c0c26d6497a287c66512861e21b4e4e437af04c117.php
@@ -27,60 +27,57 @@ class __TwigTemplate_a24742944f8139266ecc1013326fc8e4e71bd364b565e9de96dcdfda752
     protected function doDisplay(array $context, array $blocks = [])
     {
         // line 1
-        echo "  <div class=\"my-3 p-3 bg-white rounded box-shadow\">
-      <h6 class=\"border-bottom border-gray pb-2 mb-0\">Notas de desenvolvimento </h6>
-\t  \t";
-        // line 3
+        echo "    <div class=\"my-3 p-3 bg-white rounded box-shadow\">
+      <h6 class=\"border-bottom border-gray pb-2 mb-0\">Ankrahmun Updates</h6>
+      <div class=\"media text-muted pt-3\">
+        <img data-src=\"holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1\" alt=\"\" class=\"mr-2 rounded\">
+        <div class=\"media-body pb-3 mb-0 small lh-125 border-bottom border-gray\">
+          <div class=\"d-flex justify-content-between align-items-center w-100\">
+            <strong class=\"text-gray-dark\">          <span class=\"d-block\">";
+        // line 7
+        echo $this->getAttribute(($context["ticker"] ?? null), "body", []);
+        echo "</span>
+</strong>
+            <td style=\"width: 80px;\">";
+        // line 9
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute(($context["ticker"] ?? null), "date", []), "j M Y"), "html", null, true);
+        echo "</td>
+          </div>
+          <span class=\"d-block\">
+          \t  \t";
+        // line 12
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable(($context["tickers"] ?? null));
         foreach ($context['_seq'] as $context["_key"] => $context["ticker"]) {
-            // line 4
+            // line 13
             echo "\t<tr bgcolor=\"";
             echo twig_escape_filter($this->env, call_user_func_array($this->env->getFunction('getStyle')->getCallable(), [($context["i"] ?? null)]), "html", null, true);
             echo "\">
 \t\t<td style=\"width: 16px;\"><img src=\"";
-            // line 5
+            // line 14
             echo twig_escape_filter($this->env, ($context["template_path"] ?? null), "html", null, true);
             echo "/images/news/icon_";
             echo twig_escape_filter($this->env, $this->getAttribute($context["ticker"], "icon", []), "html", null, true);
             echo "_small.gif\"/></td>
-\t\t<td style=\"width: 80px;\">";
-            // line 6
-            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute($context["ticker"], "date", []), "j M Y"), "html", null, true);
-            echo "</td>
 \t\t<td>";
-            // line 7
+            // line 15
             echo $this->getAttribute($context["ticker"], "body", []);
             echo "</td>
 \t</tr>
-\t";
-            // line 9
+";
+            // line 17
             $context["i"] = (($context["i"] ?? null) + 1);
-            // line 10
-            echo "\t";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['ticker'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 11
-        echo "      <div class=\"media text-muted pt-3\">
-        <img data-src=\"holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1\" alt=\"\" class=\"mr-2 rounded\">
-        <div class=\"media-body pb-3 mb-0 small lh-125 border-bottom border-gray\">
-          <div class=\"d-flex justify-content-between align-items-center w-100\">
-            \t\t<td style=\"width: 80px;\">";
-        // line 15
-        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, $this->getAttribute(($context["ticker"] ?? null), "date", []), "j M Y"), "html", null, true);
-        echo "</td>
-          </div>
-          <span class=\"d-block\">";
-        // line 17
-        echo $this->getAttribute(($context["ticker"] ?? null), "body", []);
-        echo "</span>
+        // line 19
+        echo "          </span>
         </div>
       </div>
-<br/>
 
-   ";
+
+";
     }
 
     public function getTemplateName()
@@ -95,7 +92,7 @@ class __TwigTemplate_a24742944f8139266ecc1013326fc8e4e71bd364b565e9de96dcdfda752
 
     public function getDebugInfo()
     {
-        return array (  77 => 17,  72 => 15,  66 => 11,  60 => 10,  58 => 9,  53 => 7,  49 => 6,  43 => 5,  38 => 4,  34 => 3,  30 => 1,);
+        return array (  75 => 19,  69 => 17,  64 => 15,  58 => 14,  53 => 13,  49 => 12,  43 => 9,  38 => 7,  30 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -108,27 +105,29 @@ class __TwigTemplate_a24742944f8139266ecc1013326fc8e4e71bd364b565e9de96dcdfda752
 
     public function getSourceContext()
     {
-        return new Source("  <div class=\"my-3 p-3 bg-white rounded box-shadow\">
-      <h6 class=\"border-bottom border-gray pb-2 mb-0\">Notas de desenvolvimento </h6>
-\t  \t{% for ticker in tickers %}
-\t<tr bgcolor=\"{{ getStyle(i) }}\">
-\t\t<td style=\"width: 16px;\"><img src=\"{{ template_path }}/images/news/icon_{{ ticker.icon }}_small.gif\"/></td>
-\t\t<td style=\"width: 80px;\">{{ ticker.date|date(\"j M Y\") }}</td>
-\t\t<td>{{ ticker.body|raw }}</td>
-\t</tr>
-\t{% set i = i + 1 %}
-\t{% endfor %}
+        return new Source("    <div class=\"my-3 p-3 bg-white rounded box-shadow\">
+      <h6 class=\"border-bottom border-gray pb-2 mb-0\">Ankrahmun Updates</h6>
       <div class=\"media text-muted pt-3\">
         <img data-src=\"holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1\" alt=\"\" class=\"mr-2 rounded\">
         <div class=\"media-body pb-3 mb-0 small lh-125 border-bottom border-gray\">
           <div class=\"d-flex justify-content-between align-items-center w-100\">
-            \t\t<td style=\"width: 80px;\">{{ ticker.date|date(\"j M Y\") }}</td>
+            <strong class=\"text-gray-dark\">          <span class=\"d-block\">{{ ticker.body|raw }}</span>
+</strong>
+            <td style=\"width: 80px;\">{{ ticker.date|date(\"j M Y\") }}</td>
           </div>
-          <span class=\"d-block\">{{ ticker.body|raw }}</span>
+          <span class=\"d-block\">
+          \t  \t{% for ticker in tickers %}
+\t<tr bgcolor=\"{{ getStyle(i) }}\">
+\t\t<td style=\"width: 16px;\"><img src=\"{{ template_path }}/images/news/icon_{{ ticker.icon }}_small.gif\"/></td>
+\t\t<td>{{ ticker.body|raw }}</td>
+\t</tr>
+{% set i = i + 1 %}
+{% endfor %}
+          </span>
         </div>
       </div>
-<br/>
 
-   ", "news.tickers.html.twig", "C:\\xampp\\htdocs\\system\\templates\\news.tickers.html.twig");
+
+", "news.tickers.html.twig", "C:\\xampp\\htdocs\\system\\templates\\news.tickers.html.twig");
     }
 }
--- templates/ShadowCores/news.php
+++ templates/ShadowCores/news.php
@@ -8,31 +8,11 @@ function news_parse($title, $content, $date, $icon = 0, $author = '', $comments 
       <div class="media text-muted pt-3">
         <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
         <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-          <strong class="d-block text-gray-dark">'. $author .'</strong>
+          <strong class="d-block text-white">'. $author .'</strong>
 		  '. $content .'
         </p>
           </div>    
     </div> ';
 }
 ?>
- <div class="my-3 p-3 bg-white rounded box-shadow">
-      <h6 class="border-bottom border-gray pb-2 mb-0">Notas de desenvolvimento </h6>
-	  	'{% for ticker in tickers %}'
-	<tr bgcolor='{{ getStyle(i) }}'>
-		<td style="width: 16px;"><img src='{{ template_path }}/images/news/icon_{{ ticker.icon }}_small.gif'/></td>
-		<td style="width: 80px;">'{{ ticker.date|date("j M Y") }}'</td>
-		<td>'{{ ticker.body|raw }}'</td>
-	</tr>
-	'{% set i = i + 1 %}''
-	'{% endfor %}''
-      <div class="media text-muted pt-3">
-        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-          <div class="d-flex justify-content-between align-items-center w-100">
-            		<td style="width: 80px;">'{{ ticker.date|date("j M Y") }}'</td>
-          </div>
-          <span class="d-block">'{{ ticker.body|raw }}'</span>
-        </div>
-      </div>
-<br/>
 
